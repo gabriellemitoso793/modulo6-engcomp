00a47847f766459f51e914918894e36170ec855d
Gabrielle Mitoso <gabrielle.santos@sou.inteli.edu.br>
2024-05-05 20:31:52 -0300

    Remover diretórios do controle de versão do Git

--- ponderada-semana1/build/ola_mundo/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/gabriellemitoso/modulo6-engcomp/ponderada-semana1/install/ola_mundo'
--- ponderada-semana1/build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabriellemitoso/modulo6-engcomp/ponderada-semana1/build/turtlesim/turtlesim__py
-# Build directory: /home/gabriellemitoso/modulo6-engcomp/ponderada-semana1/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
